ee1f54585a3a43ee7baa1a6812940141ac2c146c
Mehul Saini <mehulsaini675@gmail.com>
2025-08-23 10:49:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -34,4 +35,5 @@ def move():
     return jsonify({"valid": True, "board": board})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT automatically
+    app.run(host="0.0.0.0", port=port, debug=True)
