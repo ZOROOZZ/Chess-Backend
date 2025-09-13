b92282a3fce28cab253816fc44927e973ecdd4e1
Mehul Saini <mehulsaini675@gmail.com>
2025-09-13 13:43:25 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,36 @@
-export default {
-  async fetch(request, env) {
-    let url = new URL(request.url);
-
-    if (url.pathname === "/join") {
-      let id = env.CHESSGAME.idFromName(url.searchParams.get("gameId"));
-      let obj = env.CHESSGAME.get(id);
-      return obj.fetch(request);
-    }
-
-    return new Response("Not Found", { status: 404 });
-  },
-};
-
 export class ChessGame {
   constructor(state, env) {
     this.state = state;
     this.env = env;
-    this.sessions = [];
+    this.moves = [];
   }
 
   async fetch(request) {
-    if (request.headers.get("Upgrade") === "websocket") {
-      let [client, server] = Object.values(new WebSocketPair());
-      this.sessions.push(server);
-      server.accept();
+    let url = new URL(request.url);
 
-      server.addEventListener("message", (event) => {
-        // Broadcast move to all players
-        for (let s of this.sessions) {
-          if (s !== server) {
-            s.send(event.data);
-          }
-        }
+    if (url.pathname === "/move") {
+      let body = await request.json();
+      this.moves.push(body.move);
+
+      return new Response(JSON.stringify({ ok: true, moves: this.moves }), {
+        headers: { "Content-Type": "application/json" }
       });
-
-      return new Response(null, { status: 101, webSocket: client });
     }
 
-    return new Response("OK");
+    if (url.pathname === "/state") {
+      return new Response(JSON.stringify({ moves: this.moves }), {
+        headers: { "Content-Type": "application/json" }
+      });
+    }
+
+    return new Response("ChessGame Durable Object", { status: 200 });
   }
 }
+
+export default {
+  async fetch(request, env) {
+    let id = env.ChessGame.idFromName("default");
+    let obj = env.ChessGame.get(id);
+    return obj.fetch(request);
+  }
+};
